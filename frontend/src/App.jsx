5f714a04b35c776e467a22af13ebbdbbbaf69b5c
rupeshv2121 <gp4845@myamu.ac.in>
2025-03-25 16:34:14 +0530

    unnecessary files removed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
 import './App.css'
-import Hello from './components/Hello'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Hello/>
+      
     </>
   )
 }
