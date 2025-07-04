edd209b0e8f3a1000fa7f39cd50134bca4000ada
rupeshv2121 <gp4845@myamu.ac.in>
2025-07-04 18:55:39 +0530

    Shimmer Effect Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,14 +12,14 @@ import { WishlistProvider } from './Context/WishListContext';
 import Auth from './pages/AuthPage/Auth';
 import OAuthSuccess from './pages/AuthPage/OAuthSuccess/OAuthSuccess';
 import CartPage from './pages/CartPage/CartPage';
+import ContactPage from './pages/ContactPage/ContactPage';
 import Home from './pages/Home/Home';
 import MainLayout from './pages/MainLayout/mainLayout';
 import MyOrders from './pages/MyOrders/MyOrders';
+import Profile from './pages/Profile/Profile';
 import SingleProduct from './pages/SingleProduct/SingleProduct';
 import Wishlist from './pages/Wishlist/Wishlist';
 import ProtectedRoute from './utils/ProtectedRoute';
-import Profile from './pages/Profile/Profile';
-import ContactPage from './pages/ContactPage/ContactPage';
 
 const queryClient = new QueryClient();
 
@@ -45,7 +45,7 @@ function App() {
                   <Route path='borderTest' element={<MovingBorder />} />
                   <Route path='cardRoute' element={<ProductCard />} />
                   <Route path='oauth-success' element={<OAuthSuccess />} />
-                  <Route path='contact' element={<ProtectedRoute>< ContactPage/></ProtectedRoute>} />
+                  <Route path='contact' element={<ProtectedRoute>< ContactPage /></ProtectedRoute>} />
                   <Route path='wishlist' element={<ProtectedRoute><Wishlist /></ProtectedRoute>} />
                   <Route path='profile' element={<ProtectedRoute><Profile /></ProtectedRoute>} />
                   <Route path='cart' element={
--- frontend/src/pages/MainLayout/mainLayout.jsx
+++ frontend/src/pages/MainLayout/mainLayout.jsx
@@ -1,9 +1,18 @@
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router";
 import Footer from "../../components/Footer/Footer";
-import CursorEffect from "../../components/GSAP/cursorEffect";
 import Navbar from "../../components/Navbar/Navbar";
+import Shimmer from "../../components/Shimmer/Shimmer";
 
 function MainLayout() {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => setLoading(false), 1500); // simulate fetch
+        return () => clearTimeout(timer);
+    }, []);
+
+    if (loading) return <Shimmer />;
     return (
         <>
             <Navbar />
