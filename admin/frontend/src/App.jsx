bb7f53be5c8cef5428bbfa6f7379329752b4c2b7
rupeshv2121 <gp4845@myamu.ac.in>
2025-07-27 01:22:40 +0530

    Update Stock Bug Fixed

--- admin/frontend/src/App.jsx
+++ admin/frontend/src/App.jsx
@@ -4,21 +4,20 @@ import './App.css';
 import MainLayout from './Main_Layout/MainLayout';
 import LandingPage from './Pages/LandingPage/LandingPage';
 import ProductsPage from './Pages/ProductsPage/ProductsPage';
-import Navbar from '../Components/Navbar/Navbar';
 
 function App() {
 
   return (
     <>
       <Router>
-        <ToastContainer position="top-right" autoClose={3000} style={{ zIndex: 9999 }} />
         <Routes>
           <Route path='/' element={<MainLayout />} >
             <Route index element={<LandingPage />} />
-            <Route path='/products' element={<ProductsPage/>} />
+            <Route path='/products' element={<ProductsPage />} />
           </Route>
         </Routes>
       </Router>
+      <ToastContainer position="top-right" autoClose={3000} style={{ zIndex: 9999 }} />
       {/* <MainLayout /> */}
     </>
   )
